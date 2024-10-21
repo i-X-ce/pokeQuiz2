08103aa635f7f8e028b70d93f86df5ac876bc834
i-X-ce <iceci2804@gmail.com>
2024-10-22 01:59:11 +0900

    🚚 ディレクトリの調整

--- src/app/api/test.ts
+++
@@ -1,6 +0,0 @@
-// pages/api/test.ts
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: "API is working!" });
-};
