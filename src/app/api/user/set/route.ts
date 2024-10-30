6caaa58811fe9d543a78bae4eb94c54164a861f8
i-X-ce <iceci2804@gmail.com>
2024-10-30 19:07:17 +0900

    🎨 エラーが出てたと子を修正

--- src/app/api/user/set/route.ts
+++ src/app/api/user/set/route.ts
@@ -1,9 +1,8 @@
 import connectToDatabase from "@/app/lib/conectMongoDB";
 import User from "@/app/lib/models/userModel";
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest) {
   await connectToDatabase();
   const { email, nickname } = await req.json();
   console.log(req.body);
