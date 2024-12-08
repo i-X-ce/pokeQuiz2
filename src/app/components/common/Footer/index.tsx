2880c22e4bd57aacf37b6aad41d2174b9e907ca3
i-X-ce <iceci2804@gmail.com>
2024-12-08 17:55:26 +0900

    🎨 フッターのログイン・ログアウト機能

--- src/app/components/common/Footer/index.tsx
+++ src/app/components/common/Footer/index.tsx
@@ -1,13 +1,24 @@
+"use client";
 import Link from "next/link";
 import { RogoIcon } from "../RogoIcon";
 import styles from "./style.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Footer() {
+  const session = useSession();
+
   return (
     <footer className={styles.container}>
       <RogoIcon className={styles.rogo} />
       <ul className={styles.linkContainer}>
-        <li>ログアウト</li>
+        <li
+          onClick={() => {
+            if (session.status === "authenticated") signOut();
+            else signIn("google", { prompt: "select_account" });
+          }}
+        >
+          {session.status === "authenticated" ? "ログアウト" : "ログイン"}
+        </li>
         <li>
           <Link className={styles.link} href={"/pages/terms"}>
             利用規約
