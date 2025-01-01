53057b7e3d17c3adb8a91c6e1b1d0210c73e5a70
i-X-ce <iceci2804@gmail.com>
2025-01-02 04:55:43 +0900

    🎨 ID順番通りにクイズが出題されるように

--- src/app/api/quiz/get-difficulty/route.ts
+++ src/app/api/quiz/get-difficulty/route.ts
@@ -20,21 +20,20 @@ export async function GET(req: NextRequest) {
     );
   }
 
+  const ids: string[] = req.nextUrl.searchParams.get("ids")?.split(",") || [];
+
   try {
     // eslint-disable-next-line
     let matches: any = { $match: { userId: { $ne: null } } };
     if (difficulty === "specific") {
-      const ids = req.nextUrl.searchParams
-        .get("ids")
-        ?.split(",")
-        .map((id) => new ObjectId(id));
-      if (!ids) {
+      const oids = ids.map((id) => new ObjectId(id));
+      if (!oids) {
         return NextResponse.json(
           { message: "Parameters not set correctly." },
           { status: 404 }
         );
       }
-      matches.$match._id = { $in: ids };
+      matches.$match._id = { $in: oids };
     }
 
     const questions = await Question.aggregate([
@@ -66,7 +65,12 @@ export async function GET(req: NextRequest) {
       newQuestions.sort(() => Math.random() - 0.5);
       resQuestions = newQuestions.slice(0, questionCount);
     } else if (difficulty === "specific") {
-      resQuestions = questions;
+      // 順番通りに並べる
+      const qmp = questions.reduce((acc, q) => {
+        acc[q._id] = q;
+        return acc;
+      }, {});
+      resQuestions = ids.map((id) => qmp[id]);
     } else {
       // 難易度別問題
       let filteredQuestions = questions.filter((q) => (q.answerCnt || 0) >= 10);
@@ -111,41 +115,6 @@ export async function GET(req: NextRequest) {
       }
     }
     return NextResponse.json(resQuestions, { status: 200 });
-
-    // let resQuestions: any[] = [];
-    // let mp = new Set<number>();
-    // for (let i = 0; i < Math.min(questionCount, collectionSize); i++) {
-    //   let index = 0;
-    //   const difNum =
-    //     difficulty === "hard" ? 0 : difficulty === "normal" ? 1 : 2;
-
-    //   do {
-    //     index = Math.floor(
-    //       (Math.random() * collectionSize) / 3 + (difNum * collectionSize) / 3
-    //     );
-    //     if (mp.has(index)) continue;
-    //     mp.add(index);
-    //     break;
-    //   } while (true);
-    //   let q = filteredQuestions[index];
-    //   q.userName = !q.anonymity ? q.userInfo.nickname : "けつばん";
-    //   delete q.userInfo;
-    //   delete q.correctAnswer;
-    //   delete q.description;
-    //   // delete q.userId;
-    //   if (q.img) {
-    //     const command = new GetObjectCommand({
-    //       Bucket: process.env.AWS_S3_BUCKET_NAME,
-    //       Key: "uploads/" + q._id + q.img,
-    //     });
-    //     q.img = await getSignedUrl(s3Client, command, {
-    //       expiresIn: 1800,
-    //     });
-    //   }
-    //   resQuestions.push(q);
-    // }
-
-    // return NextResponse.json(resQuestions, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
